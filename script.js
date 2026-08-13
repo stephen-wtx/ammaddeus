0cbd1e91925f5087595aead671e4c08f02b1e93b
stephen-wtx <stefeeneugenio@gmail.com>
2026-08-13 11:54:22 +0200

    update: speed

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let startPosition = 0;
 
     // const SPEED_PER_SECOND = 35;
-    const SPEED_PER_SECOND = 45;
+    const SPEED_PER_SECOND = 40;
 
 
     function animate(timestamp) {
